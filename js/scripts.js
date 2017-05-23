b21db2260d69c83da5b110856e379d3cea4fa53f
Olivia Davies <odavies@vcd-ceb131-19.eastern.ewu.edu>
2017-05-23 15:28:47 -0700

    done some good things

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,12 @@
 // @codekit-prepend "jquery.js";
 // @codekit-prepend "semantic.js";
+// @codekit-prepend "airtable.js";
 
 console.log('Hello, World!');
+
+var Airtable = require('airtable');
+Airtable.configure({
+    endpointUrl: 'https://api.airtable.com',
+    apiKey: 'keyNyK2dVB4Da7Dzo'
+});
+var base = Airtable.base('apphf0m0dkI0mIn3V');
